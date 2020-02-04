e33517e0e35007b7c21176ebc6f9b50fe593b7c6
eduassi <eduassi@gmail.com>
2020-02-04 18:54:31 -0300

    clone

---
+++ scripts/theme/theme_starter.js
@@ -0,0 +1,46 @@
+var check_all_images_complete = function(callback){
+    // We are listening for lazy images, when all images have a real size on DOM, we fire our callback function
+    let images_container = $("#content-viewer").find("img");
+    let number_images = images_container.length;
+    let progress = 0;
+    if($(images_container).length){
+        $(images_container).one("load", function() {
+            progress++;
+            if(progress >= number_images){
+                setTimeout(function(){
+                    callback();
+                    already_loaded = true;
+                },1000);
+            }
+        });   
+    }else{
+        callback();
+    }
+    
+    
+}
+
+var initial_setup = function() {
+    // Maybe we want to execute some custom scripts
+    $('[data-toggle="popover"]').popover();
+};
+
+var libraries_starter = function(){
+    // We should start our libraries here
+    refresh_objects_listeners();
+    AOS.init();
+}
+
+var load_page = function(url,callback) {
+	$("#content-viewer").load(url, function() {
+        initial_setup();
+        check_all_images_complete(libraries_starter);
+        callback();
+        $("html, body").animate({ scrollTop: 0 }, 500);
+	});
+};
+
+//abrir link
+var openUrl = function (link) {
+    window.open(link, "_blank"); 
+};
